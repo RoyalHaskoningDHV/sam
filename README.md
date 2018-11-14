f52896655c218998f30b2f79150829331726f8dd
Sebastiaan Grasdijk <sebastiaan@MacBook-Pro-van-Sebastiaan.local>
2018-11-14 11:07:49 +0100

    Hello world

---
+++ README.md
@@ -0,0 +1,20 @@
+## Version: 0.1.0
+
+# sam description
+
+The Ynformed library for smart asset management.
+
+Author: Sebastiaan Grasdijk
+Email: sebastiaan@ynformed.nl
+
+Reviewers: Fenno, Rutger, Tim
+
+# Run options
+
+``` 
+```
+
+# Testing
+
+```
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='sam',
+      version='0.1.0',
+      description='Ynformed package for smart asset management',
+      url='https://dev.ynformed.nl/diffusion/78/',
+      author='Ynformed',
+      author_email='sebastiaan@ynformed.nl',
+      license='',
+      packages=['sam'],
+      zip_safe=False,
+      install_requires=[]
+      )
