b440d0bbb080c42667a4a8b7ff165f21689b5f69
fenno <fenno@ynformed.nl>
2018-12-06 12:08:47 +0100

    Added more time utility functions, number 6,7 on T291
    Summary: created point 6 and 7 of T291, to do with wintertime and summertime
    
    Test Plan: tested locally on small dataset. No unit tests yet :O
    
    Reviewers: Sebastiaan
    
    Reviewed By: Sebastiaan
    
    Differential Revision: https://dev.ynformed.nl/D386

--- sam/utils/time.py
+++ sam/utils/time.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 
 def unit_to_seconds(unit):
     """Performs a lookup to convert a string to a number of seconds
@@ -35,3 +37,69 @@ def unit_to_seconds(unit):
         return 60 * 60 * 24 * 7
     else:
         raise ValueError("The unit is '%s', but it must start with sec, min, hour, day or week" % unit)
+
+
+def label_dst(timestamps):
+    """Find possible conflicts due to daylight savings time, by
+    labeling timestamps. This converts a series of timestamps to
+    a series of strings. The strings are either 'normal',
+    'to_summertime', or 'to_wintertime'.
+    to_summertime happens the last sunday morning of march,
+    from 2:00 to 2:59.
+    to_wintertime happens the last sunday morninig of october,
+    from 2:00 to 2:59.
+    These can be possible problems because they either happen 2
+    or 0 times. to_summertime should therefore be impossible.
+
+    Parameters
+    ----------
+    timestamps: Series, shape = (n_inputs,)
+        a series of pandas timestamps
+
+    Returns
+    -------
+    labels: string, array-like, shape = (n_inputs,)
+        a numpy array of strings, that are all either
+        'normal', 'to_summertime', or 'to_wintertime'
+    """
+    last_sunday_morning = (timestamps.dt.day >= 25) & \
+                          (timestamps.dt.weekday == 6) & \
+                          (timestamps.dt.hour == 2)
+    return np.where((last_sunday_morning) & (timestamps.dt.month == 3),
+                    "to_summertime",
+                    np.where((last_sunday_morning) & (timestamps.dt.month == 10),
+                             "to_wintertime",
+                             "normal"))
+
+
+def average_winter_time(data, tmpcol='tmp_UNID'):
+    """Solve duplicate timestamps in wintertime, by averaging them
+    Because the to_wintertime hour happens twice, there can be duplpicate timestamps
+    This function removes those duplicates by averaging the VALUE column
+    All other columns are used as group-by columns
+    
+    Parameters
+    ----------
+    data: pandas Dataframe
+        must have columns TIME, VALUE, and optionally others like ID and TYPE.
+    tmpcol: string, optional (default='tmp_UNID')
+        temporary columnname that is created in dataframe. This columnname cannot
+        exist in the dataframe already
+    
+    Returns
+    -------
+    data: pandas Dataframe
+        The same dataframe as was given in input, but with duplicate timestamps
+        removed, if they happened during the wintertime duplicate hour
+    """
+    assert tmpcol not in data.columns
+    dst_labels = label_dst(data.TIME)
+    # We make a column that is unique for all except wintertime
+    # This means that in the groupby line, non-to_wintertime
+    # lines will never be grouped
+    data[tmpcol] = np.where(dst_labels == 'to_wintertime',
+                            -1, np.arange(len(data.index)))
+    groupcols = data.columns.tolist()
+    groupcols.remove('VALUE')  # in place only
+    data = data.groupby(groupcols).mean().reset_index().drop(tmpcol, axis=1)
+    return data
