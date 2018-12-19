936c7bec1b06fd350ec6d9267409acee5bdc6680
fenno <fenno@ynformed.nl>
2018-12-19 11:50:16 +0100

    Added scorer factory for incident_recall
    Summary: added function that converts incident_recall into a scorer object for sklearn
    
    Test Plan: tested it in a gridsearch, it works
    
    Reviewers: tim
    
    Reviewed By: tim
    
    Differential Revision: https://dev.ynformed.nl/D422

--- sam/metrics/incident_recall.py
+++ sam/metrics/incident_recall.py
@@ -18,7 +18,8 @@ def incident_recall(y_true, y_pred, y_incidents, range_pred=(0, 0)):
         Predicted labels, as returned by a classifier.
     y_incidents : 1d array-like, or label indicator array / sparse matrix
         Incidents that we want to predict with the classifier within window range_pred
-    range_pred : tuple (start prediction window, end prediction window), default (0,0)
+    range_pred : tuple of int, optional (default = (0,0))
+        length of two: (start prediction window, end prediction window)
         If we want to predict an incidents 5 to 1 rows in advance, this will be (1,5)
 
     Returns
@@ -43,3 +44,34 @@ def incident_recall(y_true, y_pred, y_incidents, range_pred=(0, 0)):
     score = np.sum(incidents_found) / len(incident_indices)
 
     return score
+
+
+def make_incident_recall_scorer(range_pred=(0, 0), colname = 'incident'):
+    """
+    Wrapper around incident_recall_score, to make it an actual sklearn scorer.
+    This works by obtaining the 'incident' column from the data itself. This
+    column name is configurable. This scorer does need the incident column
+    to be present, which means by default it will be used in training the model.
+    If this is not desired, a custom model will have to be made that deletes
+    this variable before fitting/predicting.
+    
+    Parameters
+    ----------
+    range_pred : tuple of int, optional (default = (0,0))
+        length of two: (start prediction window, end prediction window)
+        Passed through to incident_recall
+    colname : string, optional (default = "indicent")
+        The column name that is given to incident_recall as incidents
+        we want to predict
+        
+    Returns
+    -------
+    scorer : a function that acts as a sklearn scorer object.
+        signature is scorer(clf, X, y), where clf is a fit model,
+        X is test data, and y is the ground truth for the test data
+    
+    """
+    def incident_recall_scorer(clf, X, y):
+        y_pred = clf.predict(X)
+        return incident_recall(y, y_pred, X[colname], range_pred)
+    return incident_recall_scorer
