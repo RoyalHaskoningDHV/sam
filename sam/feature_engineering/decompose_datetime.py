6d97613a5d430471e90a47926cc3b9ea82f2610b
Daan van Es <daan.van.es@gmail.com>
2019-04-18 16:25:42 +0200

    Conversion of cyclical features
    Summary:
    Convert cyclical features (like day of week, hour of day) to
    continuous variables, so that sunday and monday are close together
    numerically.
    See:
    - https://www.kaggle.com/avanwyk/encoding-cyclical-features-for-deep-learning
    - http://blog.davidkaleko.com/feature-engineering-cyclical-features.html
    
    Test Plan: nit test added
    
    Reviewers: rutger, Loes, fenno, tim
    
    Reviewed By: Loes
    
    Differential Revision: https://dev.ynformed.nl/D693

--- sam/feature_engineering/decompose_datetime.py
+++ sam/feature_engineering/decompose_datetime.py
@@ -1,9 +1,11 @@
 from sam.logging import log_dataframe_characteristics, log_new_columns
+import numpy as np
+import pandas as pd
 import logging
 logger = logging.getLogger(__name__)
 
 
-def decompose_datetime(df, column='TIME', components=[]):
+def decompose_datetime(df, column='TIME', components=[], cyclicals=[], remove_original=True):
     """
     Decomposes a time column to one or more components suitable as features
 
@@ -16,6 +18,15 @@ def decompose_datetime(df, column='TIME', components=[]):
     components: list
         List of components to extract from datatime column. All default pandas dt components are
         supported, and some custom functions will be implemented in the future.
+    cyclicals: list
+        Newly created .dt time variables (like hour, month) you want to convert
+        to cyclicals using sine and cosine transformations.
+        Cyclicals are variables that do not inncrease linearly, but wrap around.
+        such as days of the week and hours of the day.
+        Format is identical to components input.
+    remove_original: bool
+        whether to keep the original cyclical features (i.e. day)
+        after conversion (i.e. day_sin, day_cos)
 
     Returns
     -------
@@ -46,7 +57,6 @@ def decompose_datetime(df, column='TIME', components=[]):
     pandas_functions = [f for f in dir(df[column].dt) if not f.startswith('_')]
 
     custom_functions = []
-
     # Iterate the requested components
     for component in components:
         # Check if this is a default pandas functionality
@@ -58,7 +68,82 @@ def decompose_datetime(df, column='TIME', components=[]):
         else:
             raise NotImplementedError("Component %s not implemented" % component)
 
+    # do this before converting to cyclicals, as this has its own loggin:
     log_new_columns(result, df)
     log_dataframe_characteristics(result, logging.DEBUG)
 
+    # convert cyclicals
+    assert isinstance(cyclicals, list), 'cyclicals must be of type list'
+    if cyclicals != []:
+        result = fix_cyclical_features(result, cyclicals, remove_original, column)
+
     return(result)
+
+
+def fix_cyclical_features(df, cols, remove_original=True, column=''):
+    """
+    Convert cyclical features (like day of week, hour of day) to
+    continuous variables, so that sunday and monday are close together
+    numerically.
+    See:
+    - https://www.kaggle.com/avanwyk/encoding-cyclical-features-for-deep-learning
+    - http://blog.davidkaleko.com/feature-engineering-cyclical-features.html
+
+    Parameters
+    ----------
+    df: pandas dataframe
+        Dataframe in which the columns to convert should be present.
+    cols: list of strings
+        The column names to convert to continuous numerical values
+    remove_original: bool
+        whether to keep the original cyclical features (i.e. day)
+        after conversion (i.e. day_sin, day_cos)
+    column: string
+        name of original time column in df (e.g. TIME)
+
+    Returns
+    -------
+    new_df: pandas dataframe
+        The input dataframe with cols removed, and replaced by the
+        converted features (2 for each feature).
+    """
+
+    # add underscore to column if not empty
+    if not column == '':
+        column = column + '_'
+
+    # test inputs
+    assert isinstance(df, pd.DataFrame), 'df should be pandas dataframe'
+    assert isinstance(cols, list), 'cols should be a list of columns to convert'
+    assert isinstance(remove_original, bool)
+
+    # save copy of original dataframe
+    new_df = df.copy()
+
+    logging.debug("Sine/cosine converting cyclicals columns: %s" % (cols))
+
+    for col in cols:
+
+        # prepend column name (like TIME) to match df column names
+        col = column + col
+
+        # test whether column is in dataframe
+        assert col in df.columns, '%s is not in input dataframe' % col
+        # test colum to convert is of correct data type
+        assert df[col].dtype in [int, float], 'cyclical to convert should be integer or float'
+
+        # rescale feature so it runs from 0-2pi:
+        norm_feature = 2 * np.pi * df[col] / df[col].max()
+        # convert cyclical to 2 variables that are offset:
+        new_df[col+'_sin'] = np.sin(norm_feature)
+        new_df[col+'_cos'] = np.cos(norm_feature)
+
+        # drop the original
+        if remove_original:
+            new_df = new_df.drop(col, axis=1)
+
+    # log changes
+    log_new_columns(new_df, df)
+    log_dataframe_characteristics(new_df, logging.DEBUG)
+
+    return new_df
--- sam/feature_engineering/tests/test_decompose_datetime.py
+++ sam/feature_engineering/tests/test_decompose_datetime.py
@@ -5,9 +5,73 @@ from pandas.testing import assert_frame_equal
 import pandas as pd
 import numpy as np
 from sam.feature_engineering import decompose_datetime
+from sam.feature_engineering.decompose_datetime import fix_cyclical_features
 
 
 class TestBuildTimeFeatures(unittest.TestCase):
+
+    def test_sine_cosine_transform(self):
+        df = pd.DataFrame()
+        df['hr'] = np.arange(0, 5)
+        df_cyc = fix_cyclical_features(df.copy(), cols=['hr'])
+
+        expected = pd.DataFrame(
+            {'hr_sin': [0., 1., 0., -1., 0.],
+             'hr_cos': [1., 0., -1., 0., 1.]},
+            columns=['hr_sin', 'hr_cos'])
+        assert_frame_equal(df_cyc, expected)
+
+    def test_cyclicals(self):
+        # setup test dataframe
+        time1 = '2019/03/11 00:00:00'
+        time2 = '2019/03/11 04:00:00'
+        freq = '1h'
+        daterange = pd.date_range(time1, time2, freq=freq)
+        test_dataframe = pd.DataFrame({'TIME': daterange, 'OTHER': 1})
+
+        # add without cyclicals
+        result = decompose_datetime(test_dataframe, "TIME", ['hour'], [])
+        expected = pd.DataFrame(
+            {'TIME': daterange,
+             'OTHER': 1,
+             'TIME_hour': [0, 1, 2, 3, 4]},
+            columns=['TIME', 'OTHER', 'TIME_hour'])
+        assert_frame_equal(result, expected)
+
+        # add cyclical test without keeping original
+        result = decompose_datetime(test_dataframe, "TIME", ['hour'], ['hour'])
+        expected = pd.DataFrame(
+            {'TIME': daterange,
+             'OTHER': 1,
+             'TIME_hour_sin': [0., 1., 0., -1., 0.],
+             'TIME_hour_cos': [1., 0., -1., 0., 1.]},
+            columns=['TIME', 'OTHER', 'TIME_hour_sin', 'TIME_hour_cos'])
+        assert_frame_equal(result, expected)
+
+        # and with keep original
+        result = decompose_datetime(test_dataframe, "TIME", ['hour'], ['hour'], False)
+        expected = pd.DataFrame(
+            {'TIME': daterange,
+             'OTHER': 1,
+             'TIME_hour': [0, 1, 2, 3, 4],
+             'TIME_hour_sin': [0., 1., 0., -1., 0.],
+             'TIME_hour_cos': [1., 0., -1., 0., 1.]},
+            columns=['TIME', 'OTHER', 'TIME_hour', 'TIME_hour_sin', 'TIME_hour_cos'])
+        assert_frame_equal(result, expected)
+
+    def test_incorrect_cyclical_cols(self):
+        # setup test dataframe
+        time1 = '2019/03/11 00:00:00'
+        time2 = '2019/03/12 00:00:00'
+        freq = '2h'
+        daterange = pd.date_range(time1, time2, freq=freq)
+        test_dataframe = pd.DataFrame({'TIME': daterange, 'OTHER': 1})
+
+        # this raises an error because with input columns 'TIME',
+        # and requested cyclical conversion 'hour',
+        # the adde column is 'TIME_hour', and not 'TIME_month'
+        self.assertRaises(Exception,
+                          decompose_datetime, test_dataframe, "TIME", ['hour'], ['TIME_month'])
 
     def test_march_11(self):
         time1 = '2018/03/11 15:08:12'
