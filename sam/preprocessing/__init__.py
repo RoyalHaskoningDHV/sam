de4de93ad4974a9553d6668eaf2dab648521b20a
Daan van Es <daan.vanes@ynformed.nl>
2019-10-16 17:39:58 +0200

    added a X and y scaling function including unit test
    Summary: wrapper around sklearn transformer to fit and transform data
    
    Test Plan: unit test
    
    Reviewers: fenno, arjan, rutger
    
    Reviewed By: fenno
    
    Differential Revision: https://dev.ynformed.nl/D1105

--- sam/preprocessing/__init__.py
+++ sam/preprocessing/__init__.py
@@ -6,6 +6,7 @@ from .correct_extremes import correct_outside_range
 from .time import label_dst, average_winter_time
 from .sam_reshape import sam_format_to_wide, wide_to_sam_format
 from .rnn_reshape import RecurrentReshaper
+from .data_scaling import scale_train_test
 
 from . import correct_extremes
 from . import time
@@ -14,5 +15,5 @@ from . import sam_reshape
 __all__ = ["normalize_timestamps", "correct_above_threshold",
            "correct_below_threshold", "correct_outside_range",
            "label_dst", "average_winter_time",
-           "sam_format_to_wide", "wide_to_sam_format",
+           "sam_format_to_wide", "wide_to_sam_format", "scale_train_test",
            "RecurrentReshaper"]
