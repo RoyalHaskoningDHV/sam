fixed bugs and improved lag correlation computation
Summary:
- there was a bug that correlation type was not passed to pandas
- the plot could be made in one liner
- added best correlation lag to plot
- updated and fixed examples

Test Plan: -

Reviewers: fenno, arjan, Loes, bart

Differential Revision: https://dev.ynformed.nl/D1121

sam/exploration/lag_correlation.py
@@ -19,12 +19,14 @@ def lag_correlation(df, target_name, lag=12, method='pearson'):
         input dataframe contains variables to calculate lag correlation of
     target_name: str
         The name of the goal variable to calculate lag correlation with
-    lag: numeric, (default=12)
-       The number of lag points to calculate. By default is 12, which means
-       the correlation is calculated for lag ranging from 0 to 11
+    lag: int or list of ints (default=12)
+        When an integer is provided, a range is created from 0 to lag in steps
+        of 1, when an array of ints is provided, this is directly used.
+        Default is 12, which means the correlation is calculated for lag
+        ranging from 0 to 11.
     method: string or callable, optional (default='pearson')
-       The method used to calculate correlation. See pandas.DataFrame.corrwith.
-       Options are {‘pearson’, ‘kendall’, ‘spearman’}, or a callable.
+        The method used to calculate correlation. See pandas.DataFrame.corrwith.
+        Options are {‘pearson’, ‘kendall’, ‘spearman’}, or a callable.
 
     Returns
     -------
@@ -35,14 +37,15 @@ def lag_correlation(df, target_name, lag=12, method='pearson'):
     Examples
     --------
     >>> import pandas as pd
-    >>> from sam.exploration import create_lag_correlation
+    >>> from sam.exploration import lag_correlation
     >>> import numpy as np
     >>> X = pd.DataFrame({
-    >>>        'RAIN': [0.1, 0.2, 0.0, 0.6, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    >>>        'RAIN': [0.1, 0.2, 0.0, 0.6, 0.1, 0.0, 0.0,
+    >>>                 0.0, 0.0, 0.0, 0.0, 0.0],
     >>>        'DEBIET#A': [1, 2, 3, 4, 5, 5, 4, 3, 2, 4, 2, 3],
     >>>        'DEBIET#B': [3, 1, 2, 3, 3, 6, 4, 1, 3, 3, 1, 5]})
     >>> X['DEBIET#TOTAAL'] = X['DEBIET#A'] + X['DEBIET#B']
-    >>> tab = create_lag_correlation(df, 'DEBIET#TOTAAL', lag=11)
+    >>> tab = lag_correlation(X, 'DEBIET#TOTAAL', lag=11)
     >>> tab
         LAG  Debiet#A  Debiet#B       Rain
     0     0  0.838591  0.897340  -0.017557
@@ -56,17 +59,19 @@ def lag_correlation(df, target_name, lag=12, method='pearson'):
     8     8  0.291111  0.039253   0.871695
     9     9  0.188982  0.755929  -0.944911
     10   10  1.000000 -1.000000   1.000000
-
     """
 
     logging.debug("Now creating lag correlation with lag {}".format(lag))
     y = df[target_name]
     X = df.drop(target_name, axis=1)
 
-    RollingFeatures = BuildRollingFeatures(rolling_type='lag', window_size=np.arange(lag),
+    if np.isscalar(lag):
+        lag = np.arange(lag)
+
+    RollingFeatures = BuildRollingFeatures(rolling_type='lag', window_size=lag,
                                            lookback=0, keep_original=False)
     df = RollingFeatures.fit_transform(X)
-    corr_table = df.corrwith(y).reset_index()
+    corr_table = df.corrwith(y, method=method).reset_index()
     corr_table.columns = ['index', 'corr']
     corr_table['LAG'] = corr_table['index'].apply(lambda x: x.rsplit('_', 1)[1])
     corr_table['GROUP'] = corr_table['index'].apply(lambda x: x.rsplit('#', 1)[0])

sam/visualization/rolling_correlations.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def plot_lag_correlation(df, ylim=None):
     ''' Visualize the correlations for rolling features
 
@@ -17,36 +20,41 @@ def plot_lag_correlation(df, ylim=None):
         Examples
         --------
         >>> import pandas as pd
-        >>> from sam.feature_engineering.rolling_features import BuildRollingFeatures
-        >>> from sam.feature_selection.lag_correlation import create_lag_correlation
+        >>> from sam.feature_engineering import BuildRollingFeatures
+        >>> from sam.exploration import lag_correlation
         >>> from sam.visualization.rolling_correlations import plot_lag_correlation
         >>> import numpy as np
-        >>> goal_feature = 'DEBIET#TOTAAL_lag_0'
+        >>> goal_feature = 'DEBIET#TOTAAL'
         >>> df = pd.DataFrame({
-        >>>                'RAIN': [0.1, 0.2, 0.0, 0.6, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        >>>                'RAIN': [0.1, 0.2, 0.0, 0.6, 0.1, 0.0,
+        >>>                         0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
         >>>                'DEBIET#A': [1, 2, 3, 4, 5, 5, 4, 3, 2, 4, 2, 3],
         >>>                'DEBIET#B': [3, 1, 2, 3, 3, 6, 4, 1, 3, 3, 1, 5]})
         >>> df['DEBIET#TOTAAL'] = df['DEBIET#A'] + df['DEBIET#B']
-        >>> RollingFeatures = BuildRollingFeatures(rolling_type='lag', \\
-        >>>     window_size = np.arange(12), lookback=0, keep_original=False)
-        >>> res = RollingFeatures.fit_transform(df)
-        >>> test = create_lag_correlation(res, goal_feature)
-        >>> plot_lag_correlation(test)
+        >>> test = lag_correlation(df, goal_feature)
+        >>> f = plot_lag_correlation(test)
     '''
-    import seaborn as sns
     import matplotlib.pyplot as plt
+    import seaborn as sns
 
-    fig, ax = plt.subplots()
-    for col in df.columns:
-        if col == 'LAG':
-            continue
-        sns.lineplot(x='LAG', y=col, data=df, label=col, ax=ax)
+    fig, ax = plt.subplots(figsize=(15, 10))
+    plt.axhline(0, ls='--', c='k')
+
+    df_long = df.melt(id_vars='LAG')
+    sns.lineplot(data=df_long, x='LAG', hue='variable',
+                 y='value', ax=ax, ls='--', marker='o', ms=12)
 
     if ylim:
         ax.set_ylim(ylim)
 
+    for col in df.drop('LAG', axis=1).columns:
+        maxi = np.argmax(np.abs(df[col].values))
+        plt.plot(df['LAG'].iloc[maxi], df[col].iloc[maxi], marker='o', ms=8, c='w')
+
     ax.set_ylabel('Correlation')
     ax.legend(loc='center right',
               bbox_to_anchor=(0.35, 0.5, 1, 0))
+    sns.despine()
+    plt.tight_layout()
 
     return ax