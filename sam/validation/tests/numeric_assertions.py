c85c5ac5f7d19117dbd878b84a4b7de9e35b9fb0
Rutger Kerkhoff <rutger@ynformed.nl>
2020-09-11 15:08:48 +0200

    Shouldnt catch base exception, exception is enough
    According to the internet: https://stackoverflow.com/a/63169967/8358068
    - By catching Exception you catch most errors - basically all the errors that any module you use might throw.
    - By catching BaseException, in addition to all the above exceptions, you also catch exceptions of the types SystemExit, KeyboardInterrupt, and GeneratorExit.

--- sam/validation/tests/numeric_assertions.py
+++ sam/validation/tests/numeric_assertions.py
@@ -19,7 +19,7 @@ class NumericAssertions:
         try:
             if not np.all(np.isnan(value)):
                 self.fail(self._formatMessage(msg, standardMsg))
-        except BaseException:
+        except Exception:
             self.fail(self._formatMessage(msg, standardMsg))
 
     def assertAllNotNaN(self, value, msg=None):
@@ -30,5 +30,5 @@ class NumericAssertions:
         try:
             if np.all(~np.isnan(value)):
                 self.fail(self._formatMessage(msg, standardMsg))
-        except BaseException:
+        except Exception:
             pass
