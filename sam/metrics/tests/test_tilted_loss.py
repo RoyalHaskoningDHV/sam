5ac6fadc92e787ea2e667624108cd6b32f640095
Arjan Bontsema <arjan@ynformed.nl>
2020-09-22 17:41:19 +0200

    removed copied code

--- sam/metrics/tests/test_tilted_loss.py
+++ sam/metrics/tests/test_tilted_loss.py
@@ -10,17 +10,9 @@ class TestTiltedLoss(unittest.TestCase):
         y_true = [1, 2, 3, 4, 5]
         y_pred = [1.1, 2.1, 3.1, 3.9, 4.9]
 
-        self.assertAlmostEqual(tilted_loss(y_true, y_pred, 0.1), 0.058)
-        self.assertAlmostEqual(tilted_loss(y_true, y_pred, 0.5), 0.05)
-        self.assertAlmostEqual(tilted_loss(y_true, y_pred, 0.9), 0.042)
-
-    def test_tilted_loss_incorrect(self):
-
-        self.assertRaises(ValueError, tilted_loss, [1], [2], 1)
-        self.assertRaises(ValueError, tilted_loss, [1, 2, 3], [1, 2, 3], 0)
-        self.assertRaises(ValueError, tilted_loss, [1, 2, 3], [1, 2, 3], 1.5)
-        self.assertRaises(ValueError, tilted_loss, [1, 1, 1], [1, 2, 3], 1)
-
+        assert_almost_equal(tilted_loss(y_true, y_pred, 0.1), 0.058)
+        assert_almost_equal(tilted_loss(y_true, y_pred, 0.5), 0.05)
+        assert_almost_equal(tilted_loss(y_true, y_pred, 0.9), 0.042)
 
 if __name__ == '__main__':
     unittest.main()
