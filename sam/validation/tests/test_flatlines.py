4c198806609523d7b36cf1969b4ae22eaf69124e
Arjan Bontsema <arjan@ynformed.nl>
2020-09-28 15:00:33 +0200

    unittest

--- sam/validation/tests/test_flatlines.py
+++ sam/validation/tests/test_flatlines.py
@@ -1,6 +1,6 @@
 import unittest
 from sam.validation import RemoveFlatlines
-from .numeric_assertions import NumericAssertions
+from sam.validation.tests import NumericAssertions
 import pandas as pd
 
 
@@ -21,6 +21,41 @@ class TestRemoveExtremes(unittest.TestCase, NumericAssertions):
 
         self.assertAllNaN(data_corrected.iloc[[4, 5, 6]])
         self.assertAllNotNaN(data_corrected.drop([4, 5, 6], axis=0))
+
+    def test_remove_flatlines_auto_low(self):
+
+        # create some random data
+        data = [1, 2, 6, 3, 4, 4, 4, 3, 6, 7, 7, 2, 2]
+        test_df = pd.DataFrame()
+        test_df['values'] = data
+        # now detect flatlines with high tolerance (low pvalues)
+        cols_to_check = ['values']
+        RF = RemoveFlatlines(
+            cols=cols_to_check,
+            window='auto',
+            pvalue=1e-100)
+        data_corrected = RF.fit_transform(test_df)
+
+        # no flatlines should be detected
+        self.assertAllNotNaN(data_corrected)
+
+    def test_remove_flatlines_auto_high(self):
+
+        # create some random data
+        data = [1, 2, 6, 3, 4, 4, 4, 3, 6, 7, 7, 2, 2]
+        test_df = pd.DataFrame()
+        test_df['values'] = data
+        # now detect flatlines with low tolerance (high pvalues)
+        cols_to_check = ['values']
+        RF = RemoveFlatlines(
+            cols=cols_to_check,
+            window='auto',
+            pvalue=0.999)
+        data_corrected = RF.fit_transform(test_df)
+
+        # all flatlines should be removed
+        self.assertAllNaN(data_corrected.iloc[[4, 5, 6, 9, 10]])
+        self.assertAllNotNaN(data_corrected.drop([4, 5, 6, 9, 10], axis=0))
 
 
 if __name__ == '__main__':
