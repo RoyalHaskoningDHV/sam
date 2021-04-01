7a98b92c3d07e9048e224561cd4547b1cc6f224a
Arjan Bontsema <arjan@ynformed.nl>
2021-04-01 12:24:10 +0200

    Update of KNMI stations, current lat/lon are incorrect

--- sam/data_sources/__init__.py
+++ sam/data_sources/__init__.py
@@ -1,4 +1,5 @@
 from .synthetic_data import synthetic_timeseries, synthetic_date_range
+from .knmy_stations import knmy_stations
 from .weather import read_knmi, read_openweathermap, read_regenradar
 from .mongo_wrapper import MongoWrapper
 from .deprecated import create_synthetic_timeseries
