8f58a6c41613e9ab86d7a434f311d07545cd344c
Arjan Bontsema <arjan.bontsema@rhdhv.com>
2022-02-08 21:01:14 +0100

    typo

--- sam/models/tests/test_sam_quantile_mlp.py
+++ sam/models/tests/test_sam_quantile_mlp.py
@@ -419,7 +419,7 @@ class TestSamQuantileMLP(unittest.TestCase):
         self.assertRaises(ValueError, model.fit, self.X, self.y)
 
         # Cannot use y as feature when predict_ahead is 0
-        modesl = SamQuantileMLP(predict_ahead=0, use_y_as_feature=True, timecol="TIME")
+        model = SamQuantileMLP(predict_ahead=0, use_y_as_feature=True, timecol="TIME")
         self.assertRaises(ValueError, model.fit, self.X, self.y)
 
         # make the index of X and y not match
