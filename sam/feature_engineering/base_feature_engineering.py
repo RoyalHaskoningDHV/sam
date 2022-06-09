ab371ed9a5d0e38b041b41eae93afbcf1727706c
Arjan Bontsema <arjan.bontsema@rhdhv.com>
2022-06-09 13:36:00 +0200

    new feature engineering transformers

--- sam/feature_engineering/base_feature_engineering.py
+++ sam/feature_engineering/base_feature_engineering.py
@@ -10,41 +10,32 @@ class BaseFeatureEngineer(TransformerMixin):
 
     """
 
-    def __init__(
-        self,
-    ):
+    def __init__(self):
         pass
 
-    def feature_engineer(self, X):
-        raise NotImplementedError
+    @classmethod
+    def _from_function(cls, feature_engineer: callable):
+        """
+        Create a BaseFeatureEngineer from a function.
 
-    def fit(
-        self,
-        X,
-        y=None,
-    ):
+        This function could be any feature engineering function that takes a pandas DataFrame X 
+        and target y as input and returns a pandas DataFrame as output.
+
+        """
+        self = cls()
+        self.feature_engineer_ = feature_engineer
         return self
 
-    def transform(self, X):
+    def feature_engineer_(self, X, y=None):
+        raise NotImplementedError("You need to implement the feature_engineer method.")
+
+    def fit(self, X, y=None):
+        X_out = self.transform(X)
+        self._feature_names = X_out.columns.tolist()
+        return self
+
+    def transform(self, X, y=None):
         logging.info("Feature engineering - input shape: %s", X.shape)
-        X_out = self.feature_engineer(X)
+        X_out = self.feature_engineer_(X, y)
         logging.info("Feature engineering - output shape: %s", X_out.shape)
         return X_out
-
-
-class FeatureEngineerFromFunction(BaseFeatureEngineer):
-    """
-    Feature engineering from a function.
-
-    Parameters
-    ----------
-    feature_engineer : function
-        Function to apply to the data.
-    """
-
-    def __init__(
-        self,
-        feature_engineer: callable,
-    ):
-        super().__init__()
-        self.feature_engineer = feature_engineer
