42beca29c67b2fa4b037d356102eb844411ad72f
Arjan Bontsema <arjan.bontsema@rhdhv.com>
2022-07-29 13:48:37 +0200

    add logo to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,8 @@ from recommonmark.parser import CommonMarkParser
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
@@ -96,16 +97,21 @@ pygments_style = None
 #
 html_theme = "sphinx_rtd_theme"
 
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_logo = "sam_logo.png"
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['nstatic']
+html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
