0e4634b6d11b03835f0632cd57ac11a24eedd1d6
Adjorn <adjorn.van.engelenhoven@rhdhv.com>
2024-08-23 10:34:59 +0200

    🚨 Applied linter

--- sam/utils/score_importance.py
+++ sam/utils/score_importance.py
@@ -4,8 +4,7 @@ import numpy as np
 from sklearn.utils import check_random_state
 
 
-def iter_shuffled(X, columns_to_shuffle=None, pre_shuffle=False,
-                  random_state=None):
+def iter_shuffled(X, columns_to_shuffle=None, pre_shuffle=False, random_state=None):
     """
     Return an iterator of X matrices which have one or more columns shuffled.
     After each iteration yielded matrix is mutated inplace, so
@@ -39,26 +38,25 @@ def iter_shuffled(X, columns_to_shuffle=None, pre_shuffle=False,
         X_res[:, columns] = X[:, columns]
 
 
-def _get_scores_shufled(score_func, X, y, columns_to_shuffle=None,
-                        random_state=None):
+def _get_scores_shufled(score_func, X, y, columns_to_shuffle=None, random_state=None):
     Xs = iter_shuffled(X, columns_to_shuffle, random_state=random_state)
     return np.array([score_func(X_shuffled, y) for X_shuffled in Xs])
 
 
 def get_score_importances(
-        score_func: Callable[[Any, Any], float],
-        X: Any,
-        y: Any,
-        n_iter: int = 5,
-        columns_to_shuffle=None,
-        random_state=None):
+    score_func: Callable[[Any, Any], float],
+    X: Any,
+    y: Any,
+    n_iter: int = 5,
+    columns_to_shuffle=None,
+    random_state=None,
+):
     rng = check_random_state(random_state)
     base_score = score_func(X, y)
     scores_decreases = []
     for i in range(n_iter):
         scores_shuffled = _get_scores_shufled(
-            score_func, X, y, columns_to_shuffle=columns_to_shuffle,
-            random_state=rng
+            score_func, X, y, columns_to_shuffle=columns_to_shuffle, random_state=rng
         )
         scores_decreases.append(-scores_shuffled + base_score)
     return base_score, scores_decreases
